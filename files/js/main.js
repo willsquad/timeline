7b442f6cddef524240870d42c7d9626f91c3f7da
Ashil John <willsquad.aj@gmail.com>
2018-05-04 15:44:49 +0530

    RHS Div for Team Details Design

--- files/js/main.js
+++ files/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-   
+    
+    
+    $(document).on('click', '.graph_task_click_js', function(){
+        var self = $(this);
+        var task = self.attr('data-task');
+
+        
+    });
+
+    $(document).on('click', '.close_rhs', function(){
+        $('.timeline_rhs__sliding_rhs_div').addClass('animated slideOutRight');
+    });
 });
