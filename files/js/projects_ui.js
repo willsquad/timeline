ceba5db89824d52c55fc203001b567725aa0691f
Ashil John <willsquad.aj@gmail.com>
2018-05-14 16:15:17 +0530

    Create/Edit Project Expanding Divs added

--- files/js/projects_ui.js
+++ files/js/projects_ui.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('select').niceSelect();
+
+
+    $(document).on('click', '.content_show_hide', function(){
+        var self = $(this);
+
+        //$('.content_div_content').removeClass('show');
+        self.parent().parent().find('.content_div_content').toggleClass('show');
+
+
+    });
 });
