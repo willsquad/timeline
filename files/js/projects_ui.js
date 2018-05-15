1c74c7c3b994dafa3f8e74a6abe197a021e45443
Ashil John <willsquad.aj@gmail.com>
2018-05-15 15:47:04 +0530

    Add Task Modal Added

--- files/js/projects_ui.js
+++ files/js/projects_ui.js
@@ -4,11 +4,19 @@ $(document).ready(function(){
     $(document).on('click', '.content_show_hide, .content_title', function(){
         var self = $(this);
         var to_expand = self.attr('data-expand');
-
         //$('.content_div_content').removeClass('show');
         $('#'+to_expand).toggleClass('show');
         self.closest('.content_div').find('.expand-icon').html($('#'+to_expand).hasClass('show') ? 'remove_circle_outline' : 'add_circle_outline');
+    });
 
+    $(document).on('click', '#add_member, #add_task', function(){
 
+        $('.prompt_overlay').show();
+        $('body').addClass('overlay_applied');
+    });
+
+    $(document).on('click', '.cancel_delete, .cancel_modal', function() {
+        $('.prompt_overlay').hide();
+        $('body').removeClass('overlay_applied');
     });
 });
