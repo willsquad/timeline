ad0a6e9ccdc90c84bec98be8add9e14df470692f
Ashil John <willsquad.aj@gmail.com>
2018-05-15 13:09:36 +0530

    Transition added

--- files/js/projects_ui.js
+++ files/js/projects_ui.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
 
-    $(document).on('click', '.content_show_hide', function(){
+    $(document).on('click', '.content_show_hide, .content_title', function(){
         var self = $(this);
+        var to_expand = self.attr('data-expand');
 
         //$('.content_div_content').removeClass('show');
-        self.parent().parent().find('.content_div_content').toggleClass('show');
+        $('#'+to_expand).toggleClass('show');
+        self.closest('.content_div').find('.material-icons').html($('#'+to_expand).hasClass('show') ? 'remove_circle_outline' : 'add_circle_outline');
 
 
     });
